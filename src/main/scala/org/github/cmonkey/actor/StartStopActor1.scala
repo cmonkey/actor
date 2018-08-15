ebd60607d86f7902c419954443cc65c8ab52089f
CMonkey <42.codemonkey@gmail.com>
2018-08-15 16:42:34 +0800

    add StartStopActor1

---
+++ src/main/scala/org/github/cmonkey/actor/StartStopActor1.scala
@@ -0,0 +1,18 @@
+package org.github.cmonkey.actor
+
+import akka.actor.{Actor, Props}
+
+class StartStopActor1 extends Actor{
+  override def preStart(): Unit = {
+    println("first started")
+    context.actorOf(Props[StartStopActor2], "second")
+  }
+
+  override def postStop(): Unit = {
+    println("first stopped")
+  }
+
+  override def receive(): Receive = {
+    case "stop" => context.stop(self)
+  }
+}
