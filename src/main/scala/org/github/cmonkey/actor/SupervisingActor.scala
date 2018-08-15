f20cbc2e0fc7665a46d19b40f088a34dda06f6e0
CMonkey <42.codemonkey@gmail.com>
2018-08-15 16:52:42 +0800

    add SupervisingActor

---
+++ src/main/scala/org/github/cmonkey/actor/SupervisingActor.scala
@@ -0,0 +1,9 @@
+import akka.actor.{Actor, Props}
+
+class SupervisingActor extends Actor {
+  val child = context.actorOf(Props[SupervisedActor], "supervised-actor")
+
+  override def receive: Receive = {
+    case "FailChild" => child ! "fail"
+  }
+}
