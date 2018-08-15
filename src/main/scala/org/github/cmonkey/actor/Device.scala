ee6c0c21698ecd9c292f8736a0c71e3557a4e3b1
CMonkey <42.codemonkey@gmail.com>
2018-08-15 17:09:20 +0800

    refator Device in ReadTemperature

--- src/main/scala/org/github/cmonkey/actor/Device.scala
+++ src/main/scala/org/github/cmonkey/actor/Device.scala
@@ -1,11 +1,12 @@
 package org.github.cmonkey.actor
 
-final case object ReadTemperature(requestId: Long)
-
-final cas class RespondTemperature(requestId: Long, value: Option[Double])
-
 import akka.actor.{Actor, ActorLogging, Props}
 
 object Device{
   def props(groupId: String, deviceId: String): Props = Props(new Device(groupId, deviceId))
+
+  final case object ReadTemperature(requestId: Long)
+
+  final case class RespondTemperature(requestId: Long, value: Option[Double])
+
 }
