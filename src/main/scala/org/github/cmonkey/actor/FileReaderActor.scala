a176ab976f5e65f78248068aee70afd24d04ba9a
CMonkey <42.codemonkey@gmail.com>
2018-08-16 09:55:38 +0800

    add FileReaderActor

---
+++ src/main/scala/org/github/cmonkey/actor/FileReaderActor.scala
@@ -0,0 +1,31 @@
+package org.github.cmonkey.actor
+
+import java.io.File
+
+import akka.actor.{Actor, PoisonPill}
+import akka.event.Logging
+
+import scala.collection.mutable.ListBuffer
+import scala.io.Source
+
+class FileReaderActor extends Actor{
+  val log = Logging.getLogger(context.system,this)
+
+  override def receive: Receive = {
+
+    case f: File => {
+
+      log.info("Reading file ${f.getName} ")
+
+      val words = new ListBuffer[String]
+
+      Source.fromFile(f).getLines().foreach(line => words += line)
+
+      sender() ! words.toList
+
+      self ! PoisonPill
+    }
+
+    case _ => log.info("Still waiting for a text file")
+  }
+}
