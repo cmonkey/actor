b32c0cebb325e9a5e8bb58232459c8e05029b61e
CMonkey <42.codemonkey@gmail.com>
2018-08-23 14:46:09 +0800

    add kafka/ConsumerApp

---
+++ src/main/scala/org/github/cmonkey/actor/kafka/ConsumerApp.scala
@@ -0,0 +1,57 @@
+package org.github.cmonkey.actor.kafka
+
+import java.util.Properties
+
+import org.apache.kafka.clients.consumer.{ConsumerConfig, ConsumerRecord, ConsumerRecords, KafkaConsumer}
+import org.apache.kafka.common.serialization.{StringDeserializer, StringSerializer}
+
+import scala.collection.JavaConverters
+
+object ConsumerApp extends App{
+
+  val consumer = new Consumer(topic = "test", broker = "localhost:9092")
+
+  consumer.receiveMsg()
+
+}
+
+class Consumer(topic: String, broker:String) {
+
+  val configuration: Properties = {
+    val props = new Properties()
+
+    props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, broker)
+
+    val serializer = classOf[StringDeserializer].getCanonicalName
+    props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, serializer)
+    props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, serializer)
+    props.put(ConsumerConfig.GROUP_ID_CONFIG, "1")
+
+    props
+  }
+
+  val consumer = new KafkaConsumer[String, String](configuration)
+  val subscribers = List[String](topic)
+  consumer.subscribe(JavaConverters.asJavaCollection(subscribers))
+
+  var isPoll = true
+
+  def isPollMsg(): Boolean = {
+    isPoll
+  }
+
+  def receiveMsg(): Unit = {
+
+    while(isPollMsg()) {
+      val records: ConsumerRecords[String, String] = consumer.poll(1000)
+
+      records.forEach(record => {
+        println(s"record mesg = ${record}")
+      })
+    }
+  }
+
+  def shudownPollMsg(): Unit = {
+    isPoll = false
+  }
+}
