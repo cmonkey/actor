f087f48444e70e5c179a2511a30cbdc2016d4055
CMonkey <42.codemonkey@gmail.com>
2018-08-30 15:58:21 +0800

    add remote/LocalActorApp

--- src/main/scala/org/github/cmonkey/actor/remote/LocalActorApp.scala
+++ src/main/scala/org/github/cmonkey/actor/remote/LocalActorApp.scala
@@ -1,5 +1,14 @@
 package org.github.cmonkey.actor.remote
 
-class LocalActorApp {
+import akka.actor.{ActorSystem, Props}
+
+object LocalActorApp extends App{
+
+  val actorSystem = ActorSystem("local-system")
+
+  val localActor = actorSystem.actorOf(Props[LocalActor], "localActor")
+
+  localActor ! Init
+  localActor ! SendNoReturn
 
 }
