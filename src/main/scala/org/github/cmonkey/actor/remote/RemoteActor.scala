1c53c9cad4bd597f12c0de652bbad04d553fd45c
CMonkey <42.codemonkey@gmail.com>
2018-08-30 15:58:30 +0800

    add remote/RemoteActor

--- src/main/scala/org/github/cmonkey/actor/remote/RemoteActor.scala
+++ src/main/scala/org/github/cmonkey/actor/remote/RemoteActor.scala
@@ -1,5 +1,12 @@
 package org.github.cmonkey.actor.remote
 
-class RemoteActor {
+import akka.actor.Actor
 
+class RemoteActor extends Actor{
+  override def receive: Receive = {
+    case msg: String =>
+      println(s"RemoteActor receive msg = ${msg}")
+
+      sender() ! "Hello from the RemoteActor"
+  }
 }
