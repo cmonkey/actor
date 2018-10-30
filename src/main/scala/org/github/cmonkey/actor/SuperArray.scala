ce98dee00e5a731921810f948b1a520bc5e382ff
CMonkey <42.codemonkey@gmail.com>
2018-10-30 12:02:42 +0800

    add scala SuperArray

--- src/main/scala/org/github/cmonkey/actor/SuperArray.scala
+++ src/main/scala/org/github/cmonkey/actor/SuperArray.scala
@@ -1,5 +1,17 @@
 package org.github.cmonkey.actor
 
-class SuperArray {
+class SuperArray(val size:Long) {
+
+  val BYTE = 1
+
+  var address: Long = UnsafeUtils.getUnsafe().allocateMemory(size * BYTE)
+
+  def set(i: Long, value: Byte): Unit = {
+    UnsafeUtils.getUnsafe().putByte(address + i * BYTE, value)
+  }
+
+  def get(idx: Long): Int = {
+    UnsafeUtils.getUnsafe().getByte(address * idx * BYTE)
+  }
 
 }
