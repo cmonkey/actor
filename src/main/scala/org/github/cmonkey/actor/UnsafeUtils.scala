40c9dd37bc1f4c2a360ca1392dfd331add319e76
CMonkey <42.codemonkey@gmail.com>
2018-10-30 12:02:52 +0800

    add UnsafeUtils

--- src/main/scala/org/github/cmonkey/actor/UnsafeUtils.scala
+++ src/main/scala/org/github/cmonkey/actor/UnsafeUtils.scala
@@ -1,5 +1,20 @@
 package org.github.cmonkey.actor
 
+import sun.misc.Unsafe
+
 class UnsafeUtils {
 
+  def getUnsafe(): Unsafe = {
+
+    val field = classOf[Unsafe].getDeclaredField("theUnsafe")
+    field.setAccessible(true)
+
+    field.get(null).asInstanceOf[Unsafe]
+  }
+}
+
+object UnsafeUtils{
+  val utils = new UnsafeUtils
+
+  def getUnsafe() = utils.getUnsafe()
 }
