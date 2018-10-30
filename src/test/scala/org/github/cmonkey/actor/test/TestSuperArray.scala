1e5381d0e8da26b86258cadf4c9a5945adac37a6
CMonkey <42.codemonkey@gmail.com>
2018-10-30 12:03:12 +0800

    add TestSuperArray scala

--- src/test/scala/org/github/cmonkey/actor/test/TestSuperArray.scala
+++ src/test/scala/org/github/cmonkey/actor/test/TestSuperArray.scala
@@ -1,5 +1,22 @@
 package org.github.cmonkey.actor.test
 
-object TestSuperArray {
+import org.github.cmonkey.actor.SuperArray
+
+object TestSuperArray extends App{
+
+  val superSize: Long  = Integer.MAX_VALUE
+
+  val superArray= new SuperArray(superSize)
+
+  println(s"array size = ${superArray.size}")
+
+  var sum = 0
+  val num = 10
+  (1 to 100).foreach(i => {
+    superArray.set(num + i, 3.toByte)
+    sum += superArray.get(num + i)
+  })
+
+  println(s"sum = ${sum}")
 
 }
