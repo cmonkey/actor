7bba57d85c46b9734338e3225439a2c910130ea2
CMonkey <42.codemonkey@gmail.com>
2018-10-30 12:03:20 +0800

    add TestUnsafe

--- src/test/scala/org/github/cmonkey/actor/test/TestUnsafe.scala
+++ src/test/scala/org/github/cmonkey/actor/test/TestUnsafe.scala
@@ -1,5 +1,15 @@
 package org.github.cmonkey.actor.test
 
-object TestUnsafe {
+import java.lang.reflect.Field
+
+import sun.misc.Unsafe
+
+object TestUnsafe extends App{
+
+  val theUnsafeField:Field = classOf[Unsafe].getDeclaredField("theUnsafe")
+  println(theUnsafeField)
+  theUnsafeField.setAccessible(true)
+  val unsafe:Unsafe = theUnsafeField.get(null).asInstanceOf[Unsafe]
+  println(unsafe)
 
 }
