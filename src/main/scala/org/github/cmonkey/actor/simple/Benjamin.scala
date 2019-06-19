10134513c7ca1355a53d0fe0b64c9a0e9f869450
CMonkey <42.codemonkey@gmail.com>
2019-06-19 11:12:27 +0800

    add Benjamin

--- src/main/scala/org/github/cmonkey/actor/simple/Benjamin.scala
+++ src/main/scala/org/github/cmonkey/actor/simple/Benjamin.scala
@@ -1,5 +1,20 @@
 package org.github.cmonkey.actor.simple
 
-class Benjamin {
+import akka.actor.{Actor, ActorRef}
 
+class Benjamin extends Actor{
+  override def receive: Receive = {
+    case friend: ActorRef => {
+      val name = friend.path.name
+      println(s"sender path name = ${name}")
+      friend ! s"Hello ${name} !"
+    }
+
+    case msg: String => {
+      println(msg)
+      context.system.terminate()
+    }
+
+    case _ =>
+  }
 }
